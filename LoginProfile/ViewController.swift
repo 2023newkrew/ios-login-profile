3e0159464431609f68f2e8062a3af0cf5358a8cd
volgaHwang <wltkak2@gmail.com>
2023-01-03 11:28:27 +0900

    feat: ID 5글자 입력 시, 로그인 버튼 색상 변경 구현
    - idTextField에 addTarget 사용해서, textFieldDidChange를 사용하게 구현했습니다.

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -5,14 +5,33 @@
 // 
 
 import UIKit
+/*
+ String Text 만들어서 매핑?
+ StoryBoard 사용해서 하는 방식?
+ */
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var findAccountOrPasswordButton: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()        
+        idTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
     }
-
-
+    
+    @objc private func textFieldDidChange(_ sender: Any?) {
+        guard let idText = idTextField.text else { return }
+        if idText.count >= 5 {
+            signInButton.backgroundColor = .systemYellow
+        } else {
+            signInButton.backgroundColor = .systemGray6
+        }
+    }
 }
-
