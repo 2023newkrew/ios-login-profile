f0617376e595beaf4b7f7590f03500d0a7a13490
john.lim <limjae96@gmail.com>
2023-01-03 11:58:22 +0900

    feat: 로그인 화면 텍스트 필드 underline 추가

--- LoginProfile/ViewController.swift
+++ LoginProfile/ViewController.swift
@@ -7,12 +7,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    enum Constant {
+        static let sideSpace = CGFloat(16)
+        static let underlineSpace = CGFloat(8.5)
+    }
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        drawUnderline(of: idTextField)
+        drawUnderline(of: passwordTextField)
     }
-
-
+    
+    func drawUnderline(of textField: UITextField){
+        let border = CALayer()
+        let underlineWidth = self.view.frame.width - Constant.sideSpace * 2
+        border.frame = CGRect(x: 0, y: textField.frame.size.height + Constant.underlineSpace, width: underlineWidth, height: 1)
+        border.backgroundColor = UIColor.placeholderText.cgColor
+        textField.layer.addSublayer(border)
+    }
 }
 
