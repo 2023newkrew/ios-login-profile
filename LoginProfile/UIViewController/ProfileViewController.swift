701d54038d45f17a626265ebacb958e9e41c6e6c
kakao <kakao@summer-love-pc.local>
2023-01-05 11:44:59 +0900

    feat: Close Button 구현

--- LoginProfile/UIViewController/ProfileViewController.swift
+++ LoginProfile/UIViewController/ProfileViewController.swift
@@ -8,9 +8,52 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    enum Constant {
+        static let closeButtonSize = CGFloat(22)
+        static let closeButtonLeftSpace = CGFloat(22)
+        static let closeButtonTopSpace = CGFloat(20) + (closeButtonLeftSpace/2)
+    }
+    
+    private var closeButton: UIButton = CloseButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.white
+        self.view.backgroundColor = UIColor.red
+        
+        self.closeButton.addTarget(self, action: #selector(closeButtonTouched(sender:)), for: .touchUpInside)
+        
+        self.configureCloseButton()
+    }
+}
+
+extension ProfileViewController {
+    var safeAreaLeadingAnchor: NSLayoutXAxisAnchor {
+        return self.view.safeAreaLayoutGuide.leadingAnchor
+    }
+    var safeAreaTrailingAnchor: NSLayoutXAxisAnchor {
+        return self.view.safeAreaLayoutGuide.trailingAnchor
+    }
+    var safeAreaTopAnchor: NSLayoutYAxisAnchor {
+        return self.view.safeAreaLayoutGuide.topAnchor
+    }
+    var safeAreaBottomAnchor: NSLayoutYAxisAnchor {
+        return self.view.safeAreaLayoutGuide.bottomAnchor
+    }
+    
+    func configureCloseButton() {
+        self.closeButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.closeButton)
+        NSLayoutConstraint.activate([
+            self.closeButton.leadingAnchor.constraint(equalTo: self.safeAreaLeadingAnchor, constant: Constant.closeButtonLeftSpace),
+            self.closeButton.widthAnchor.constraint(equalToConstant: Constant.closeButtonSize),
+            self.closeButton.heightAnchor.constraint(equalToConstant: Constant.closeButtonSize),
+            self.closeButton.centerYAnchor.constraint(equalTo: self.safeAreaTopAnchor, constant: Constant.closeButtonTopSpace)
+        ])
+    }
+}
+
+extension ProfileViewController {
+    @objc func closeButtonTouched(sender: UIButton) {
+        self.dismiss(animated: true)
     }
 }
