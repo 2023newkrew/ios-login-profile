97df797e19623728f496befbbb29aa1eb0c1d1cf
volgaHwang <wltkak2@gmail.com>
2023-01-05 16:25:17 +0900

    feat: ProfileViewController AutoLayout 구현
    - AutoLayout을 Sub Views에 적용했습니다.
    - ProfileViewController에서 setLayout, setHierarchy, setAttributes를 사용해서 뷰들을 조정하게 구현했습니다.

--- LoginProfile/ProfileViewController.swift
+++ LoginProfile/ProfileViewController.swift
@@ -9,19 +9,17 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    let upperMenuStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fill
-        stackView.distribution = .equalCentering
-        return stackView
-    }()
-    
     let lowerMenuStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fill
         stackView.distribution = .equalCentering
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = .init(top: 20,
+                                                   leading: 50,
+                                                   bottom: 0,
+                                                   trailing: 50)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
@@ -34,63 +32,122 @@ class ProfileViewController: UIViewController {
         let image = UIImage(named: "loopy")
         let imageView = UIImageView(image: image)
         imageView.layer.cornerRadius = 5
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     let profileLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "루피"
         return label
     }()
     
     let seperatorView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    let chatButton: UIButton = {
-        let button = UIButton()
-        let image = UIImage(named: "btn_bubble")
-        button.setImage(image, for: .normal)
-        return button
+    let chatTouchableView: TouchableView = {
+        let view = TouchableView()
+        view.setImage(name: "btn_bubble")
+        view.setLabel(title: "1:1 채팅")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
-    let chatLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        return label
+    let callTouchableView: TouchableView = {
+        let view = TouchableView()
+        view.setImage(name: "btn_phone")
+        view.setLabel(title: "통화하기")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let storyTouchableView: TouchableView = {
+        let view = TouchableView()
+        view.setImage(name: "btn_quote")
+        view.setLabel(title: "카카오스토리")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
-    let callButton: UIButton = {
-        let button = UIButton()
-        let image = UIImage(named: "btn_phone")
-        button.setImage(image, for: .normal)
-        return button
-    }()
-    
-    let callLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        return label
-    }()
-    
-    let storyButton: UIButton = {
-        let button = UIButton()
-        let image = UIImage(named: "btn_qoute")
-        button.setImage(image, for: .normal)
-        return button
-    }()
-    
-    let storyLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        return label
-    }()
-        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setAttributes()
+        setHierarchy()
+        setLayout()
+    }
+    
+    private func setAttributes() {
+        view.backgroundColor = .red
         
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        favouriteButton.translatesAutoresizingMaskIntoConstraints = false
+        wonButton.translatesAutoresizingMaskIntoConstraints = false
+        giftButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setHierarchy() {
+        [chatTouchableView, callTouchableView, storyTouchableView].forEach { touchableView in
+            lowerMenuStackView.addArrangedSubview(touchableView)
+        }
+        
+        [closeButton, giftButton, wonButton,
+         favouriteButton, profileImageView, profileLabel,
+         seperatorView, lowerMenuStackView].forEach { subView in
+            view.addSubview(subView)
+        }
+    }
+    
+    private func setLayout() {
+        // Todo: 레이아웃 정의
+        NSLayoutConstraint.activate([
+            closeButton.widthAnchor.constraint(equalToConstant: 22),
+            closeButton.heightAnchor.constraint(equalToConstant: 22),
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                             constant: 20),
+            closeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                                                 constant: 22),
+            
+            favouriteButton.widthAnchor.constraint(equalToConstant: 26),
+            favouriteButton.heightAnchor.constraint(equalToConstant: 26),
+            favouriteButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                                 constant: 18),
+            favouriteButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                                                      constant: -16),
+            
+            wonButton.widthAnchor.constraint(equalToConstant: 26),
+            wonButton.heightAnchor.constraint(equalToConstant: 26),
+            wonButton.trailingAnchor.constraint(equalTo: favouriteButton.leadingAnchor, constant: -12),
+            wonButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                           constant: 18),
+            
+            giftButton.widthAnchor.constraint(equalToConstant: 26),
+            giftButton.heightAnchor.constraint(equalToConstant: 26),
+            giftButton.trailingAnchor.constraint(equalTo: wonButton.leadingAnchor, constant: -12),
+            giftButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                           constant: 18),
+            
+            profileImageView.heightAnchor.constraint(equalToConstant: 100),
+            profileImageView.widthAnchor.constraint(equalToConstant: 100),
+            profileImageView.topAnchor.constraint(equalTo: giftButton.bottomAnchor, constant: 395),
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            profileLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 5),
+            profileLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            seperatorView.topAnchor.constraint(equalTo: profileLabel.bottomAnchor, constant: 51),
+            seperatorView.heightAnchor.constraint(equalToConstant: 1),
+            seperatorView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            seperatorView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            lowerMenuStackView.topAnchor.constraint(equalTo: seperatorView.bottomAnchor),
+            lowerMenuStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            lowerMenuStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 }
